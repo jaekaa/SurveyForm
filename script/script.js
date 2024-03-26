69031811a5ce5b2581d7a4cf96d8885d86494d11
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-03-26 10:02:39 +0800

    Fixed Bug

--- script/script.js
+++ script/script.js
@@ -1,62 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const submitButton = document.querySelector('.submit-button');
+    const surveyForm = document.getElementById('surveyForm');
+    const messageContainer = document.getElementById('messageContainerSingle');
 
-    // Add click event listener to the submit button
-    submitButton.addEventListener('click', function() {
-        // Toggle 'active' class to animate the circle
-        submitButton.classList.toggle('active');
-
-        // Reset animation after a short delay
-        setTimeout(function() {
-        submitButton.classList.toggle('active');
-        }, 300); // Adjust the delay time as needed (in milliseconds)
+    // Add submit event listener to the form
+    surveyForm.addEventListener('submit', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
     });
 
     const radioInputs = document.querySelectorAll('.input-radio');
     let optionSelected = false;
 
-    radioInputs.forEach(function (input) {
-        input.addEventListener('mouseover', function () {
+    radioInputs.forEach(function(input) {
+        input.addEventListener('mouseover', function() {
             if (!optionSelected) {
                 input.checked = true;
             }
         });
 
-        input.addEventListener('click', function () {
+        input.addEventListener('click', function() {
             optionSelected = true;
-            radioInputs.forEach(function (otherInput) {
+            radioInputs.forEach(function(otherInput) {
                 if (otherInput !== input) {
                     otherInput.checked = false;
                 }
             });
         });
     });
-
-    checkboxInputs.forEach(function (input) {
-        input.addEventListener('mouseover', function () {
-            if (!optionSelected) {
-                input.checked = true;
-            }
-        });
-
-        input.addEventListener('mouseout', function () {
-            if (!optionSelected) {
-                input.checked = false;
-            }
-        });
-
-        input.addEventListener('click', function () {
-            if (input.checked) {
-                optionSelected = true;
-                checkboxInputs.forEach(function (otherInput) {
-                    if (otherInput !== input) {
-                        otherInput.checked = false;
-                    }
-                });
-            } else {
-                optionSelected = false;
-            }
-        });
-    });
 });
-        
